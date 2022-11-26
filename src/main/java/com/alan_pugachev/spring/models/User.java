d954f5bd49c7ea1ac7183dbd2d4b5fc6010a2978
alanpugachev <aszneesizzle@gmail.com>
2022-11-26 21:27:10 +0300

    add User model and make some chenges in .html file

---
+++ src/main/java/com/alan_pugachev/spring/models/User.java
@@ -0,0 +1,63 @@
+package com.alan_pugachev.spring.models;
+
+public class User {
+    private int id;
+    private String username;
+    private String firstName;
+    private String secondName;
+    private String email;
+
+
+
+    //constructors, setters and getters
+    public User() {
+    }
+
+    public User(int id, String username, String firstName, String secondName, String email) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
