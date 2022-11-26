0388cfce60841507fcf101ba64ca0c51572d48f3
alanpugachev <aszneesizzle@gmail.com>
2022-11-26 16:15:12 +0300

    Repair some issues and add home page

---
+++ src/main/java/com/alan_pugachev/spring/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.alan_pugachev.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping()
+    public String homePage(){
+        return "home/main";
+    }
+}
